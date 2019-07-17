331d1f5c012060d25493d22a8a69072dc9a484d3
CryptoMountains <40394451+CryptoMountains@users.noreply.github.com>
2019-07-17 23:50:35 +0200

    update settings

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from binance.client import Client
 
 # Binance Client
-client = Client('YQbgwBT5mTbFGduQ5VuSc23Hy4PEwnzBG31fDg8badxLeKEVynTeeUazwB7yawQ6', 'Pi9f7eLgb4SnkejwxOaVtBUkqKiXcwgGD62SxjgfzvTYo65DYzWibQaOsj3zCHlv')
+client = Client('YOUR_API_KEY', 'YOUR_SECRET_KEY')
 symbol = 'BTCUSDT'
 
 # Preprocessing 
@@ -9,7 +9,6 @@ data_inizio = '01.01.2019 00:00:00,00'
 data_fine = '30.05.2019 00:00:00,00'
 
 # model
-
 model_name = 'Gini'
 model_name_2 = 'Entropy'
 
